940f765c4648dcb07da613fad6a0f7715726568b
az-m <alisonzmills@gmail.com>
2025-05-15 16:56:52 +0100

    routine: EOD

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-//globals
+// globals
 
 let counter = 0;
 let upgradeDataLocal = [];
 let upgradePurchases = {};
 let timer;
 
+// listeners
+
 document.getElementById("clicker").addEventListener("click", () => {
   counter = counter + 1;
   document.getElementById("counter").textContent = counter;
 });
 
+// actions
+
 theAutomatedBit();
+
+// functions
 
 async function theAutomatedBit() {
   const upgradeData = await getUpgrades();
@@ -19,12 +25,11 @@ async function theAutomatedBit() {
   makeUpgradeElements(upgradeData);
   upgradesClick();
 
-  console.log(upgradeDataLocal);
-
   timer = setInterval(timerHandler, 1000);
 }
 
 async function getUpgrades() {
+  // go go get upgrade item info
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
   );
@@ -37,6 +42,7 @@ function setUpgradeDataLocal(upgradeArr) {
 }
 
 function makeUpgradeElements(upgradeArr) {
+  // creates the div for each upgrade item, populates it with the name and cost, and adds it to the upgrades section in the dom
   upgradeArr.forEach((item, index) => {
     const el = document.createElement("div");
     el.setAttribute("class", "upgrade");
@@ -58,21 +64,33 @@ function makeUpgradeElements(upgradeArr) {
 }
 
 function upgradesClick() {
+  // for each of the upgrade div elements we created we want a separate click event, so lets get them all and go through them
   const upgradeDivs = document.getElementById("upgrades").children;
   for (let div of upgradeDivs) {
     div.addEventListener("click", () => {
       let id = div.getAttribute("id");
-      if (id in upgradePurchases) {
-        upgradePurchases[id]++;
-      } else {
-        upgradePurchases[id] = 1;
+
+      //we can only purchase an item if we have enough 'cookies' so
+      let price = upgradeDataLocal[id - 1].cost;
+      if (counter >= price) {
+        // did we already buy one of these? if not, add a new key for it otherwise increment number we own
+        if (id in upgradePurchases) {
+          upgradePurchases[id]++;
+        } else {
+          upgradePurchases[id] = 1;
+        }
+        // update our purchases in the local storage
+        localStorage.setItem(
+          "upgradesStored",
+          JSON.stringify(upgradePurchases)
+        );
       }
-      localStorage.setItem("upgradesStored", JSON.stringify(upgradePurchases));
     });
   }
 }
 
 function timerHandler() {
+  // every second, we check for purchases, add up the total increases contributed by the items, and add the total to the overall 'cookie' count
   n = 0;
   for (i = 1; i <= upgradeDataLocal.length; i++) {
     if (i in upgradePurchases) {
@@ -85,8 +103,16 @@ function timerHandler() {
 }
 
 resetButton.addEventListener("click", () => {
+  // this resets all progress back to 0
   localStorage.clear();
   upgradePurchases = {};
+  counter = 0;
   document.getElementById("perSec").textContent = 0;
   document.getElementById("counter").textContent = 0;
+});
+
+document.getElementById("stopTimer").addEventListener("click", () => {
+  //this is just to stop it running while I style things or just want it to stop. take it out later.
+  clearInterval(timer);
+  timer = null;
 });
